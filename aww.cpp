b7afa22c45925b9e044693b72f95161e8d07e8ec
Vinnie <sheepking4444@gmail.com>
2025-02-07 07:54:30 -0800

    realize that I don't need to use ceil because I'm already casting this jawn as an int

--- aww.cpp
+++ aww.cpp
@@ -4,29 +4,36 @@
 #include <FL/Fl_Select_Browser.H>
 #include <string>
 #include <iostream>
-#include <cmath>
 
 int PARENT_WIDTH = 500;
 int PARENT_HEIGHT = 500;
+int PARENT_MARGIN_X = 10;
+int PARENT_MARGIN_Y = 10;
 int WIFI_BROWSER_WIDTH = PARENT_WIDTH - 20;
-int WIFI_BROWSER_HEIGHT = ceil(PARENT_HEIGHT / 2); // ensure we are using a whole number
-int WIFI_BROWSER_OFFSET_X = 10;
-int WIFI_BROWSER_OFFSET_Y = 10;
+int WIFI_BROWSER_HEIGHT = PARENT_HEIGHT / 2;
+int WIFI_BROWSER_OFFSET_X = PARENT_MARGIN_X;
+int WIFI_BROWSER_OFFSET_Y = PARENT_MARGIN_Y;
+int WIFI_SCAN_BUTTON_WIDTH = 150; 
+int WIFI_SCAN_BUTTON_HEIGHT = 30;
+int WIFI_SCAN_BUTTON_OFFSET_X = PARENT_MARGIN_X;
+int WIFI_SCAN_BUTTON_OFFSET_Y = WIFI_BROWSER_HEIGHT + 20;
 const char * APPNAME = "Arch Wifi Wrapper";
 const char * WIFI_LIST = "Wifi List";
+const char * WIFI_SCAN = "Scan for Networks";
 
 int main(int argc, char **argv) {
 	Fl_Window *window = new Fl_Window(PARENT_WIDTH, PARENT_HEIGHT, APPNAME);
  	Fl_Select_Browser browser(WIFI_BROWSER_OFFSET_X, 
 				  WIFI_BROWSER_OFFSET_Y,
 				  WIFI_BROWSER_WIDTH,
-				  WIFI_BROWSER_HEIGHT, 
-				  WIFI_LIST);
-	browser.add("fartmannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
-	for(int i = 0; i < 30; i++)
-	{
-		browser.add("chungus");
-	}
+				  WIFI_BROWSER_HEIGHT);
+
+	Fl_Button *button = new Fl_Button(WIFI_SCAN_BUTTON_OFFSET_X,
+					 WIFI_SCAN_BUTTON_OFFSET_Y,
+					 WIFI_SCAN_BUTTON_WIDTH,
+					 WIFI_SCAN_BUTTON_HEIGHT,
+					 WIFI_SCAN);
+
 	window->end();
 	window->show(argc, argv);
 	return Fl::run();
