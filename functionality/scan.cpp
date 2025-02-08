8f2ffd9432c45e17040da244422b123b312ac037
Vinnie <sheepking4444@gmail.com>
2025-02-07 18:13:51 -0800

    get scan actually working and populating the browser

--- functionality/scan.cpp
+++ functionality/scan.cpp
@@ -2,16 +2,20 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Select_Browser.H>
 #include <string>
+#include <sstream>
 #include "run.h"
 #include "../constants/constants.h"
 
 void scan(Fl_Widget* w, void* p)
 {
 	Fl_Select_Browser* browser = ((Fl_Select_Browser*)p);
-
+	browser->clear();
 	std::string scan_output = run_command(CMD_SCAN_WIFI);
-	std::cout << scan_output << std::endl;
-	//browser->add("hi");
-	//parse_wifi_list("fart");
+	std::istringstream stream(scan_output); 
+    std::string line;
+    
+    while(std::getline(stream, line)) {
+		browser->add(line.c_str());
+    }
 	return;
 }
