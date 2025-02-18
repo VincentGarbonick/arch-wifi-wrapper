ef751ba4ac98097610d2f930ace3a6dbf6f2f07f
Vinnie <sheepking4444@gmail.com>
2025-02-17 17:02:03 -0800

    add  connect base files

---
+++ functionality/connect.h
@@ -0,0 +1,18 @@
+#ifndef CONNECT_H
+#define CONNECT_H
+
+#include <string>
+
+using std::string;
+
+namespace connect
+{
+    struct network_data {
+        string bssid;
+        string ssid;
+    };
+
+
+}
+
+#endif
